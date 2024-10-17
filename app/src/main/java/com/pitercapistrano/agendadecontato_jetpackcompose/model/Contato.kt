a5035a1ef36d1b95944f0171eebcc71d7948da90
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-16 22:42:48 -0300

    Telas, Componentes e Constantes Criadas!

---
+++ app/src/main/java/com/pitercapistrano/agendadecontato_jetpackcompose/model/Contato.kt
@@ -0,0 +1,14 @@
+package com.pitercapistrano.agendadecontato_jetpackcompose.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import com.pitercapistrano.agendadecontato_jetpackcompose.constantes.Constantes
+
+
+@Entity(tableName = Constantes.TABELA_CONTATOS)
+data class Contato(
+    @ColumnInfo(name = "nome") val nome: String,
+    @ColumnInfo(name = "sobrenome") val sobrenome: String,
+    @ColumnInfo(name = "email") val email: String,
+    @ColumnInfo(name = "telefone") val telefone: String,
+)
